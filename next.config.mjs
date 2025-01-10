b8ee263bbe74c5e0e19628210dc021abf16ae558
code016hiro <nagatotuan@gmail.com>
2025-01-10 10:33:58 +0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hnfetimxhezkhdgcdshw.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yvmawpqkjjlmicbjjcum.supabase.co',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
